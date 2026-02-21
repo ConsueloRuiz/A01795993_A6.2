1c2d468813d3eef0da73f282fe18a13c044ce003
Consuelo Ruiz <A01795993@tec.mx>
2026-02-20 22:49:31 -0600

    version v1.2

--- hotel_system/tests/test_hotel.py
+++ hotel_system/tests/test_hotel.py
@@ -23,3 +23,29 @@ class TestHotelService(unittest.TestCase):
         self.service.reserve_room("H2")
         result = self.service.get_hotel("H2")
         self.assertEqual(result["available_rooms"], 4)
+
+    def test_get_hotel_not_found(self):
+        result = self.service.get_hotel("NO_EXIST")
+        self.assertIsNone(result)
+
+    def test_delete_hotel_not_found(self):
+        self.service.delete_hotel("NO_EXIST")
+        hotels = self.service.storage.load()
+        self.assertEqual(hotels, [])
+
+    def test_update_hotel_not_found(self):
+        self.service.update_hotel("NO_EXIST", name="Nuevo")
+        hotels = self.service.storage.load()
+        self.assertEqual(hotels, [])
+
+    def test_reserve_no_rooms(self):
+        hotel = Hotel("H9", "Full Hotel", 1, 0)
+        self.service.create_hotel(hotel)
+        self.service.reserve_room("H9")
+
+        result = self.service.get_hotel("H9")
+        self.assertEqual(result["available_rooms"], 0)
+
+    def test_cancel_reservation_no_hotel(self):
+        self.service.cancel_reservation("NO_EXIST")
+        self.assertEqual(self.service.storage.load(), [])
